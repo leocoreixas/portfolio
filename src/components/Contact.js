bc31d63b2f4db9e789eb3523c08b888171991f70
Leonardo Coreixas <lcoreixas@provertec.com.br>
2024-03-07 19:36:53 -0300

    fix: update contact me

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,11 @@ import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import Alert from 'react-bootstrap/Alert';
-//import sendEmail from "../services/send-email/send-email";
+import { Envelope, Whatsapp } from 'react-bootstrap-icons';
 
 
 export const Contact = () => {
+  const whatsappMessage = "Olá, %20vim%20pelo%20portifólio%20e%20gostaria%20de%20saber%20mais%20sobre!";
   const formInitialDetails = {
     firstName: '',
     lastName: '',
@@ -41,7 +42,6 @@ export const Contact = () => {
     setLoading(true);
 
     try {
-      debugger
       setFormDetails(formInitialDetails);
       const message = `Name: ${formDetails.firstName} ${formDetails.lastName} \nEmail: ${formDetails.email} \nPhone: ${formDetails.phone} \nMessage: ${formDetails.message}`;
       //const received = await sendEmail(formDetails.email, { message });
@@ -78,38 +78,24 @@ export const Contact = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <h2>Get In Touch</h2>
-                  <form onSubmit={handleSubmit}>
-                    <Row>
-                      <Col size={12} sm={6} className="px-1">
-                        <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
-                      </Col>
-                      <Col size={12} className="px-1">
-                        <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                        <button type="submit" disabled={loading || !validateForm()}><span>{buttonText}</span></button>
-                      </Col>
-                      {
-                        status.message &&
-                        <Alert
-                          className="alert-button"
-                          variant={status.succes ? 'success' : 'danger'}
-                          onClose={() => setStatus({})}
-                          dismissible
-                          closeLabel="Close Alert"
-                        >
-                          <p>{status.message}</p>
-                        </Alert>
-                      }
-                    </Row>
-                  </form>
+                  <Row className="mb-3 align-items-center">
+                    <Col xs={1} className="px-0">
+                      <Envelope className="fs-4" />
+                    </Col>
+                    <Col xs={11} className="px-1">
+                      <p className="mb-0">Send me a message:</p>
+                      <a href="mailto:leonardocoreixas@gmail.com" className="contact-link">leonardocoreixas@gmail.com</a>
+                    </Col>
+                  </Row>
+                  <Row className="align-items-center">
+                    <Col xs={1} className="px-0">
+                      <Whatsapp className="fs-4" />
+                    </Col>
+                    <Col xs={11} className="px-1">
+                      <p className="mb-0">Contact me:</p>
+                      <a href={`https://wa.me/5521967329230?text=${whatsappMessage}`} className="contact-link">+55 21 96732 9230</a>
+                    </Col>
+                  </Row>
                 </div>}
             </TrackVisibility>
           </Col>
